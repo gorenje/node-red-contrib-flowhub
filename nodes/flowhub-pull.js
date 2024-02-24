1d4c0cbc95a749693e06d5d5b07a44cac3100a80
Gerrit Riessen <gerrit@openmindmap.org>
2024-02-24 22:28:29 +0100

    removed email and reintroduced API token

--- nodes/flowhub-pull.js
+++ nodes/flowhub-pull.js
@@ -1,62 +1,88 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
   function FlowHubPullFunctionality(config) {
-    RED.nodes.createNode(this,config);
+    RED.nodes.createNode(this, config);
 
     var node = this;
     var cfg = config;
 
-    node.on('close', function() {
+    let obtainFlow = (msg,token) => {
+      if (cfg.flowid || msg.flowid) {
+        import('got').then((module) => {
+          module.got.get(
+            "https://api.flowhub.org/v1/flows/" + (cfg.flowid || msg.flowid) + 
+            "?cb=" + new Date().getTime() + "&v=" + (cfg.flowrevision || msg.flowrevision),
+          {
+              headers: {
+                "FlowHub-API-Version": "brownbear",
+                "X-FHB-TOKEN": token
+              },
+              timeout: {
+                request: 25000,
+                response: 25000
+              }
+          }).then(resp => {
+
+              try {
+                var payload = JSON.parse(resp.body)
+
+                if (!Array.isArray(payload)) {
+                  node.status({ fill: "red", shape: "dot", text: "access denied" });
+                  return node.error("access denied")
+                }
+
+                if (cfg.notab) {
+                  payload = payload.filter((nde) => {
+                    return nde.type != "tab";
+                  })
+                }
+
+                node.send({
+                  ...msg,
+                  payload: payload
+                });
+
+              } catch (err) {
+                node.status({ fill: "red", shape: "dot", text: "Response Failed" });
+                setTimeout(() => { node.status({}); }, 2500)
+                node.error(err)
+                return
+              }
+
+            }).catch(err => {
+              node.status({ fill: "red", shape: "dot", text: "Failed" });
+              setTimeout(() => { node.status({}); }, 2500)
+              node.error(err)
+            });
+        });
+      } else {
+        node.status({ fill: "yellow", shape: "dot", text: "No FlowId defined" });
+        setTimeout(() => { node.status({}); }, 2500)
+      }
+    }
+
+    node.on('close', function () {
       node.status({});
     });
 
-    node.on("input", function(msg, send, done) {
-      if ( cfg.flowid || msg.flowid ) {
+    node.on("input", function (msg, send, done) {
+      let cfgNodes = []
 
-        import('got').then( (module) => {
-          module.got.get( "https://api.flowhub.org/v1/flows/" + (
-            cfg.flowid || msg.flowid
-          ) + "?cb=" + new Date().getTime() + "&v=" + (
-            cfg.flowrevision || msg.flowrevision
-          ), 
-          {
-            headers: {
-              "FlowHub-API-Version": "brownbear",
-            },
-            timeout: {
-              request: 25000,
-              response: 25000
-            }
-          }).then( resp => {
-
-            try {
-              var payload = JSON.parse( resp.body )
-
-              if ( cfg.notab ) {
-                payload = payload.filter( (nde) => {
-                  return nde.type != "tab";
-                })
-              }
-
-              node.send({
-                ...msg,
-                payload: payload
-              });
-            } catch (err) {
-              node.status({fill:"red",shape:"dot",text:"Response Failed"});
-              setTimeout( () => { node.status({}); }, 2500)
-              node.error({...msg, error: err})
-              return
-            }
-
-          }).catch( err => {
-            node.status({fill:"red",shape:"dot",text:"Failed"});
-            setTimeout( () => { node.status({}); }, 2500)
-            node.error({...msg, error: err})
-          });
-        });
+      RED.nodes.eachNode(nde => {
+        if (nde.type == "FlowHubCfg") {
+          cfgNodes.push(nde)
+        }
+      })
+      
+      if ( cfgNodes.length > 0 ) {
+        RED.util.evaluateNodeProperty(cfgNodes[0].apiToken, cfgNodes[0].apiTokenType, node, msg, (err, result) => {
+          if ( !err ) {
+            obtainFlow(msg, result)
+          } else {
+            obtainFlow(msg, "")
+          }
+        })
       } else {
-        node.status({fill:"yellow",shape:"dot",text:"No FlowId defined"});
-        setTimeout( () => { node.status({}); }, 2500)
+        obtainFlow(msg,"")
       }
     });
   }
--- nodes/flowhubcfg.js
+++ nodes/flowhubcfg.js
@@ -118,35 +118,41 @@ module.exports = function (RED) {
         if (req.body && req.body.flowdata && req.body.flowid) {
           var msg = req.body;
 
-          import('got').then((module) => {
-            module.got.post("https://api.flowhub.org/v1/diff", {
-              headers: {
-                "FlowHub-API-Version": "brownbear",
-              },
-              json: {
-                flowid: msg.flowid,
-                flowdata: msg.flowdata,
-                flowlabel: msg.flowlabel,
-              },
-              timeout: {
-                request: 25000,
-                response: 25000
-              }
-            }).then(resp => {
-              try {
-                res.status(200).send(JSON.parse(resp.body));
-              } catch (err) {
-                res.sendStatus(500);
-              }
+          var cfgnode = req.body.cfgnode;
+          var node = RED.nodes.getNode(cfgnode.id)
+
+          RED.util.evaluateNodeProperty(cfgnode.apiToken, cfgnode.apiTokenType, node, msg, (err, result) => {
+            import('got').then((module) => {
+              module.got.post("https://api.flowhub.org/v1/diff", {
+                headers: {
+                  "FlowHub-API-Version": "brownbear",
+                  "X-FHB-TOKEN": result
+                },
+                json: {
+                  flowid: msg.flowid,
+                  flowdata: msg.flowdata,
+                  flowlabel: msg.flowlabel,
+                },
+                timeout: {
+                  request: 25000,
+                  response: 25000
+                }
+              }).then(resp => {
+                try {
+                  res.status(200).send(JSON.parse(resp.body));
+                } catch (err) {
+                  res.sendStatus(500);
+                }
+              }).catch(err => { res.sendStatus(500); });
             }).catch(err => { res.sendStatus(500); });
-          }).catch(err => { res.sendStatus(500); });
+          })
         } else {
           res.sendStatus(405);
         }
       } catch (err) {
         res.sendStatus(500);
       }
-    });
+  });
 
 
   RED.httpAdmin.post("/FlowHubPush",
@@ -185,4 +191,32 @@ module.exports = function (RED) {
     }
   );
 
+  RED.httpAdmin.post("/FlowHubToken",
+    RED.auth.needsPermission("flowhub.write"),
+    (req, res) => {
+      try {
+        if (req.body) {
+          var msg = req.body;
+          var cfgnode = req.body.cfgnode;
+          var node = RED.nodes.getNode(cfgnode.id)
+
+          RED.util.evaluateNodeProperty(cfgnode.apiToken, cfgnode.apiTokenType, node, msg, (err, result) => {
+            if (err || (result || "").trim() == "") {
+              res.sendStatus(404);
+            } else {
+              if ( result.startsWith("fhb_") ) {
+                res.status(200).send({token: result});
+              } else {
+                res.sendStatus(404);
+              }
+            }
+          });
+        } else {
+          res.sendStatus(405);
+        }
+      } catch (err) {
+        res.sendStatus(500);
+      }
+    }
+  );
 }
